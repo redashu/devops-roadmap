845989b221a01187431d2e5f69b43df73fc56e7d
Ashutoshh <asashuthegreat4@gmail.com>
2023-04-05 14:31:39 +0530

    Create user_input.sh

---
+++ shell-scripting/user_input.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+read a
+echo "Welcome to Bash scripting "
+sleep 2 
+# echo "Welcome to Centos 8 "
+sleep 1
+<<EOF
+echo "checking Os version please wait.."
+sleep 1
+ cat /etc/os-release   |   grep  -Eiw 'name|version'
+
+EOF
+
+# user input 
+echo "please enter any command to run ..."
+# taking input and storing into a variable 
+read x
+
+echo "You have entered  $x"
+echo "Now we are running user input ..plz wait..."
+sleep 1
+$x
